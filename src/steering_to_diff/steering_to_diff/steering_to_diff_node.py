49f20f07f589be11776ad9a5d589fb43c4be86ff
Highsky7 <albert31115@gmail.com>
2025-09-07 20:43:14 +0900

    fixed : fixed steering parameter

--- src/steering_to_diff/steering_to_diff/steering_to_diff_node.py
+++ src/steering_to_diff/steering_to_diff/steering_to_diff_node.py
@@ -36,9 +36,9 @@ class SteeringToDiff(Node):
         # ===== 기본값(요청하신 값 반영) =====
         self.declare_parameter('wheelbase', 0.60)             # L [m]
         self.declare_parameter('track_width', 0.516)          # W [m]
-        self.declare_parameter('v_nominal', 0.55)             # [m/s]
+        self.declare_parameter('v_nominal', 0.4)             # [m/s]
         self.declare_parameter('max_speed', 0.55)             # [m/s]
-        self.declare_parameter('kappa_gain', 1.0)
+        self.declare_parameter('kappa_gain', 2.0)
         self.declare_parameter('max_steering_angle', 1.5)    # [rad]
         self.declare_parameter('use_speed_topic', False)
         self.declare_parameter('control_hz', 50.0)
